f0b970a5df0d1b4757a386dae44a60dbe5896a0d
novyteai <novyte00@gmail.com>
2025-12-17 11:32:11 -0500

    feat.

---
+++ crates/hotpath/src/lib.rs
@@ -0,0 +1,11 @@
+#![deny(clippy::disallowed_types)]
+#![deny(warnings)]
+
+pub mod audit;
+pub mod config_atomic;
+pub mod control_safety;
+pub mod executor;
+pub mod homeostasis;
+pub mod proposer;
+pub mod spsa;
+pub mod telemetry;
